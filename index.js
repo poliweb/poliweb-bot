9b98efc6979d0bbbf6287a1fb34f9b7cb946e1ea
Pavel Polistovskiy <poliweb007@gmail.com>
2022-08-01 10:59:19 +0600

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,32 @@ bot.command('portfolio', (ctx) => {
     ]
 })
 
+bot.command('html', (ctx) => {
+    return ctx.replyWithHTML(`
+    <b>bold</b>, <strong>bold</strong>
+    <i>italic</i>, <em>italic</em>
+    <u>underline</u>, <ins>underline</ins>
+    <s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
+    <span class="tg-spoiler">spoiler</span>, <tg-spoiler>spoiler</tg-spoiler>
+    <b>bold <i>italic bold <s>italic bold strikethrough <span class="tg-spoiler">italic bold strikethrough spoiler</span></s> <u>underline italic bold</u></i> bold</b>
+    <a href="http://www.example.com/">inline URL</a>
+    <a href="tg://user?id=123456789">inline mention of a user</a>
+    <code>inline fixed-width code</code>
+    <pre>pre-formatted fixed-width code block</pre>
+    <pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
+    <pre>/help</pre>
+    <b>Фрагмент кода на JS для этого чат-бота</b>
+    <i>Я люблю писать код и узнавать новинки в IT индустрии</i>
+    <a href="tg://user?id=1234567890">Моя философия  - <i>обучение на протяжении всей жизни</i></a>
+    <code>bot.command('start', (ctx) => {return ctx.replyWithHTML('Это старт')})</code>
+    <pre><code class="language-js">
+    bot.command('start', (ctx) => {
+        return ctx.replyWithHTML('Это старт')
+      })
+    </code></pre>
+    `)
+})
+
 
 /**
  * Start bot
